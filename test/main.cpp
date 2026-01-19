316f78bae23a6bccec6e424442ac23f812aacb10
hjc2000 <hjc.666@foxmail.com>
2026-01-19 17:00:59 +0800

    ``` fix(test): 修正测试文件中的路径和函数调用
    - 将测试路径从 "C:/Users/huang/dev/.temp/1.png" 修改为 "C:/Users/huang/dev/.temp/test"
    - 将 base::filesystem::RemoveReadOnlyAttribute 函数调用更正为
      base::filesystem::RemoveReadOnlyAttributeRecursively
    ```

--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-	base::Path path = "C:/Users/huang/dev/.temp/1.png";
-	base::filesystem::RemoveReadOnlyAttribute(path);
+	base::Path path = "C:/Users/huang/dev/.temp/test";
+	base::filesystem::RemoveReadOnlyAttributeRecursively(path);
 
 	return 0;
 }
